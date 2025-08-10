beffe748028a388fda572abc9c77b48dbc83ab17
Max Montesino <montesinopasiempre@gmail.com>
2025-08-09 20:14:58 -0500

    troubleshooting env variables issue

---
+++ news_search.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_CSE_ID = os.getenv("GOOGLE_CSE_ID")
+print(f"GOOGLE_API_KEY: {GOOGLE_API_KEY}")
+print(f"GOOGLE_CSE_ID: {GOOGLE_CSE_ID}")
+
+SEARCH_TERMS = ["Adobe"]
+DATE_RESTRICTION = "d1" #restricts results to last 24 hours
+MAX_RESULTS = 100 #100 results per day is max for free tier of API
+
+def run_search(query):
+    """Fetch paginated results from Google Custom Search API"""
+    results = []
+    for start_index in range(1, MAX_RESULTS + 1, 10):
+        url = "https://www.googleapis.com/customsearch/v1"
+        params = {
+            "key": GOOGLE_API_KEY,
+            "cx": GOOGLE_CSE_ID,
+            "q": query,
+            "start": start_index,
+            "sort": "date",
+            "dateRestrict": DATE_RESTRICTION
+        }
+        r = requests.get(url, params=params)
+        r.raise_for_status()
+        data = r.json()
+        items = data.get("items", [])
+        if not items:
+            break
+        results.extend(items)
+    return results
+
+def main():
+    all_new_results = {}
+
+    for query in SEARCH_TERMS:
+        try:
+            all_new_results[query] = run_search(query)
+        except Exception as e:
+            print(f"Error with query '{query}': {e}")
+            #all_new_results[query] = {}
+
+    if all_new_results:
+        print(all_new_results)
+    else:
+        print("No new results found.")
+
+if __name__ == "__main__":
+    print("running!")
+    main()

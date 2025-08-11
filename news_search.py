b51d2ba9363ef18b6155d3bd8b5662b8f850d081
Max Montesino <montesinopasiempre@gmail.com>
2025-08-10 21:02:50 -0500

    added fields to params in run_saerch, more exception handling logic in main

--- news_search.py
+++ news_search.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import HTTPError
 import json
 import datetime
 import os
@@ -8,17 +9,16 @@ load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 GOOGLE_CSE_ID = os.getenv("GOOGLE_CSE_ID")
-
 SEARCH_TERMS = [
     "adobe -\"Adobe Mountain\"", 
-    "adobe -\"Adobe style\"", 
+    "adobe -\"Adobe style\"",
     "adobe -\"Adobe Style\"", 
     "adobe -\"Adobe-style\"", 
     "adobe -\"Adobe-Style\"", 
     "adobe -\"Adobe home\""
     ]
 DATE_RESTRICTION = "d1" #restricts results to last 24 hours
-MAX_RESULTS = 100 #100 results per day is max for free tier of API
+MAX_RESULTS = 100 #100 results per query is max for free tier of API
 
 def run_search(query):
     """Fetch paginated results from Google Custom Search API"""
@@ -28,6 +28,7 @@ def run_search(query):
         params = {
             "key": GOOGLE_API_KEY,
             "cx": GOOGLE_CSE_ID,
+            "fields": "items(title,link,displayLink,snippet)",
             "q": query,
             "start": start_index,
             "sort": "date",
@@ -48,6 +49,11 @@ def main():
     for query in SEARCH_TERMS:
         try:
             all_new_results[query] = run_search(query)
+        except HTTPError as err:
+            if err.response.status_code == 429: # Too Many Requests, CSE limits to 100 queries per day
+                print(f"Too many requests, skipping query '{query}'. Please try again tomorrow.")
+            else:
+                print(f"HTTP error {err.response.status_code} with query '{query}': {err}")
         except Exception as e:
             print(f"Error with query '{query}': {e}")
 

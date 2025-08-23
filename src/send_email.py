4a8a418e99d174df7f9b37d03352cad552b4138f
Max Montesino <montesinopasiempre@gmail.com>
2025-08-22 21:13:43 -0500

    removed dupe send_email, added format_email_body and send_email functions to send_email module and calls in news_search/main

--- src/send_email.py
+++ src/send_email.py
@@ -10,10 +10,29 @@ EMAIL_SENDER = os.getenv("EMAIL_SENDER")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_RECIPIENT = os.getenv("EMAIL_RECIPIENT")
 
-test_string = 'This is a test email.\nLet me know if you got it.\nThanks.'
-smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-smtpObj.ehlo()
-smtpObj.starttls()
-smtpObj.login(EMAIL_SENDER, EMAIL_PASSWORD)
-smtpObj.sendmail(EMAIL_SENDER, EMAIL_RECIPIENT, test_string)
-smtpObj.quit()
+def format_email_body(results):
+    content = "<html>\n<body>"
+    for query, items in results.items():
+        if not items:
+            continue
+        content += f"\n<h2>Results for query '{query}':</h2>\n<ul>" + "\n".join(
+                f"<li>{item['displayLink']}:<a href='{item['link']}'>{item['title']}</a>\n<p><em>{item['snippet']}</em></p></li>" for item in items
+            )
+    content += "\n</ul>\n</body>\n</html>"
+    if content == "<html>\n<body>\n</ul>\n</body>\n</html>":
+        return "No new results found."
+    return content 
+
+def send_email(content):
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.login(EMAIL_SENDER, EMAIL_PASSWORD)
+    smtpObj.sendmail(EMAIL_SENDER, EMAIL_RECIPIENT, content)
+    smtpObj.quit()
+
+"""
+TO-DO: 
+Where do I add MIME module?
+TEST both functions
+"""

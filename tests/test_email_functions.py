aac042527fa1f4b2e57b6d86fe02eb35d036fc85
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2025-08-23 14:39:40 -0500

    Refactor email function test for clarity

--- tests/test_email_functions.py
+++ tests/test_email_functions.py
@@ -28,8 +28,7 @@ def test_send_email_no_results(mocker: MockerFixture):
     email_functions.send_email("No new results found.")
 
     #Assert that send_email returns correct parameters
-    args, _ = mock_smtp.return_value.sendmail.call_args
-    from_addr, to_addr, msg_str = args
+    from_addr, to_addr, msg_str = mock_smtp.return_value.sendmail.call_args[0]
 
     assert from_addr == "fake_email_sender"
     assert to_addr == "fake_email_recipient"
